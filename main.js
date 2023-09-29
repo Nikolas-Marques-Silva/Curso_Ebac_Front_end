d511eb1a4ebbaffc74a1694f10f071b7acd90e49
Nikolas-M-Silva <Nikolas.Marques.Silva@gmail.com>
2023-09-29 14:46:53 -0300

    feat(main.js): form slide down

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
+    $('header button').click(function() {
+        $('form').slideDown()
+    })
+
+    $('#cancelButton').click(function() {
+        $('form').slideUp()
+    })
+
     $('form').on('submit', function(e) {
         e.preventDefault()
     })
