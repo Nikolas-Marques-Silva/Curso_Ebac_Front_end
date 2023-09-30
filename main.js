254776f332cf165dc921a4e346096cf7a3a13795
Nikolas-M-Silva <Nikolas.Marques.Silva@gmail.com>
2023-09-30 13:57:07 -0300

    feat(main.js): agora podemos adicionar fotos

--- main.js
+++ main.js
@@ -9,5 +9,12 @@ $(document).ready(function() {
 
     $('form').on('submit', function(e) {
         e.preventDefault()
+        const enderecoImagem = $('enderecoImagem').val()
+        const novoItem = $('<li style="display: none"></li>')
+        $(`<img src="${enderecoImagem}">`).appendTo(novoItem)
+        $(`<div class="overlayLinkImagem"><a href="${enderecoImagem}" target="_blank" title="Ver imagem em tamanho real">Ver imagem em tamanho real</a></div>`).appendTo(novoItem)
+        $(novoItem).appendTo('ul')
+        $(novoItem).fadeIn(1000);
+        $(enderecoImagem).val('')
     })
 })
