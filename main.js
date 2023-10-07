83e8837255a99cb4a4eb6da161e2c023a8c9971e
Nikolas-M-Silva <Nikolas.Marques.Silva@gmail.com>
2023-10-07 16:13:14 -0300

    feat(main.js): melhorando o formulário

--- main.js
+++ main.js
@@ -3,4 +3,39 @@ $(document).ready(function() {
     $('.menu-hamburguer').click(function() {
         $('nav').slideToggle(1000)
     });
+    $('.telefone').mask('(00) 00000-0000', {
+        placeholder: '(__) _____-____'
+    });
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            menssagem: {
+                required: true
+            },
+            veiculoInteresse: {
+                required: false
+            }
+        },
+        messages: {
+            nome: "Por favor, insira seu nome"
+        },
+        submitHandler: function(form) {
+            console.log(form);
+        },
+        invalidHandler: function(event, validator) {
+            let camposIncorretos = validator.numberOfInvalids();
+            if (camposIncorretos) {
+                alert(`Existem ${camposIncorretos} não preenchidos`)
+            }
+        }
+    });
 });
