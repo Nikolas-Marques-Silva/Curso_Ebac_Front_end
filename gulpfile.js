367c2e05515c94ce06493251c8bdda29132a75be
Níkolas Marques da Silva <nikolas.marques.silva@gmail.com>
2023-11-20 14:02:04 -0300

    feat(gulpfile.js):sequencia paralela

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,23 @@
 const gulp = require('gulp')
 
 function funcaoPadrao(callback) {
-  console.log('Executando via gulp:')
-  callback()
+  setTimeout(() => {
+    console.log('Executando via gulp:')
+    callback()
+  }, 1000);
 }
 
 function dizOI(callback) {
-  console.log('Olá Gulp')
-  dizTchau()
-  callback()
+  setTimeout(() => {
+    console.log('Olá Gulp')
+    dizTchau()
+    callback()
+  }, 2000);
 }
 
 function dizTchau() {
   console.log('Tchau Gulp')
 }
 
-exports.default = gulp.series(funcaoPadrao, dizOI)
+exports.default = gulp.parallel(funcaoPadrao, dizOI)
 exports.dizOI = dizOI
