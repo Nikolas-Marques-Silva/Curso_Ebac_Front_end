82adcd2e04451cd334487a612859450340a58b8c
Níkolas Marques da Silva <nikolas.marques.silva@gmail.com>
2023-11-21 16:03:29 -0300

    feat(modulo_16): Gulp com sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,17 @@
 const gulp = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+const sourcemaps = require('gulp-sourcemaps')
+
+
+function compilaSass() {
+  return gulp.src('./source/styles/main.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }))
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest('./build/styles'))
+}
 
 function funcaoPadrao(callback) {
   setTimeout(() => {
@@ -21,3 +34,7 @@ function dizTchau() {
 
 exports.default = gulp.parallel(funcaoPadrao, dizOI)
 exports.dizOI = dizOI
+exports.sass = compilaSass
+exports.watch = function() {
+  gulp.watch('./source/styles/*.scss', {ignoreInitial: false}, gulp.series(compilaSass))
+}
