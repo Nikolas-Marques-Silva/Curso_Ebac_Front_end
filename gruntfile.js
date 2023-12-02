a680990bf59a218b971ef514707f528f670f3025
Níkolas Marques da Silva <nikolas.marques.silva@gmail.com>
2023-12-02 13:39:28 -0300

    feat(gruntfile.js):funções assincronas

--- gruntfile.js
+++ gruntfile.js
@@ -2,4 +2,12 @@ module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON("package.json"),
   });
+  grunt.registerTask("olaGrunt", function () {
+    const done = this.async();
+    setTimeout(function () {
+      console.log("Olá Grunt");
+      done();
+    }, 3000);
+  });
+  grunt.registerTask("default", ["olaGrunt"])
 };
