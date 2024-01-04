ace546d56f66abde25b1a8e9f19c603c1ef8a4bb
Níkolas Marques da Silva <nikolas.marques.da.silva1@gmail.com>
2024-01-04 17:05:36 -0300

    feat(funcionario.js):polimorfismo

--- funcionario.js
+++ funcionario.js
@@ -24,14 +24,41 @@ function Funcionario (nome, sobrenome, cargo, salorio) {
         console.log(`Eu trabalho como ${this.cargo}`)
     }
 
+    this.increaseSalario = () => {
+        const newSalario = _salario * 1.1
+        _salario = newSalario
+    }
+
     Pessoa.call(this, nome, sobrenome)
 }
 
+function Estagiario(nome, sobrenome) {
+    Funcionario.call(this, nome, sobrenome, 'Estagiário', 1000)
+
+    this.increaseSalario = () => {
+        const newSalario = this.getSalario() * 1.07
+        this.setSalario(newSalario) 
+    }
+}
+
+function Gerente(nome, sobrenome) {
+    Funcionario.call(this, nome, sobrenome, 'Gerente', 10000)
+
+    this.increaseSalario = () => {
+        const newSalario = this.getSalario() * 1.2
+        this.setSalario(newSalario)
+    }
+}
+
 const funcionario1 = new Funcionario('Maria', 'Silva', 'Desenvolvedora', 2000)
+const funcionario2 = new Estagiario('João', 'Silva')
+const funcionario3 = new Gerente('Pedro', 'Silva')
 
-funcionario1.dizOi()
-funcionario1.dizCargo()
-
-funcionario1.setSalario(2500)
-
+funcionario1.increaseSalario()
 console.log(funcionario1.getSalario())
+
+funcionario2.increaseSalario()
+console.log(funcionario2.getSalario())
+
+funcionario3.increaseSalario()
+console.log(funcionario3.getSalario())
