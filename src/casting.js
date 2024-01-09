7418ac39427afae54f4a6c11505af9e11e1dcb45
Níkolas Marques da Silva <nikolas.marques.da.silva1@gmail.com>
2024-01-09 14:18:30 -0300

    feat(casting.ts): Casting

---
+++ src/casting.js
@@ -0,0 +1,9 @@
+"use strict";
+var casting;
+(function (casting) {
+    let idade = 25;
+    idade.toFixed();
+    idade.length;
+    idade.pop();
+    let nome = 35;
+})(casting || (casting = {}));
