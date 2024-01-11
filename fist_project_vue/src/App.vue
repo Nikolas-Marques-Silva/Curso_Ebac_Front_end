bb862af30ac97007595e512f6e56aff28cc2e370
Níkolas Marques da Silva <nikolas.marques.da.silva1@gmail.com>
2024-01-11 03:29:37 -0300

    feat(App.vue):Começando no Vue

--- fist_project_vue/src/App.vue
+++ fist_project_vue/src/App.vue
@@ -1,47 +1,27 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+  const nome = "Gian"
+  const meuObj = {
+    nome: "Gian",
+    filmeFavorito: "Cidade de Deus"
+  }
+  
+  function dizOi(){
+    return `${nome} diz oi`;
+  }
+
+  const imagem = "https://i.kym-cdn.com/photos/images/newsfeed/002/237/352/7d5.png"
+
+  const botaoEstaDesabilitado = false
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>{{ nome }}</h1>
+  <h1>{{ meuObj.filmeFavorito }}</h1>
+  <h1>{{ dizOi() }}</h1>
+  <img :src="imagem" alt="">
+  <button :disabled="botaoEstaDesabilitado">Clique aqui</button>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
