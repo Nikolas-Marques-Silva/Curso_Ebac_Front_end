c5c78ebf31d1f7dd773d392f5670d5bc387a91a4
Níkolas Marques da Silva <nikolas.marques.da.silva1@gmail.com>
2024-01-15 12:07:03 -0300

    feat(modulo_27):Componentes

--- todo-vue/src/App.vue
+++ todo-vue/src/App.vue
@@ -1,5 +1,9 @@
 <script setup>
   import { reactive, computed } from 'vue';
+  import Header from './components/Header.vue';
+  import Formulario from './components/Formulario.vue';
+  import Lista from './components/Lista.vue';
+
   const state = reactive({
     tasks: [
       {
@@ -53,38 +57,12 @@
 
 <template>
   <div class="container">
-    <header class="p-5 mb-4 mt-4 bg-light rounded-3">
-      <h1>Minhas tarefas</h1>
-      <p>Você possui {{ getPendentTasks.length }} tarefas pendentes</p>
-    </header>
-    <form @submit.prevent="registerTask" action="">
-      <div class="row">
-        <div class="col">
-          <input :value="state.tempTask" @change="event => state.tempTask = event.target.value" required type="text" name="" id="" class="form-control" placeholder="Digite aqui a descrição da sua tarefa">
-        </div>
-        <div class="col-md-2">
-          <button type="submit" class="btn btn-primary">Cadastrar</button>
-        </div>
-        <div class="col-md-2">
-          <select @change="event => state.filter = event.target.value" name="" id="" class="form-control">
-            <option value="todas">Todas as tarefas</option>
-            <option value="pendentes">Pendentes</option>
-            <option value="finalizadas">Finalizadas</option>
-          </select>
-        </div>
-      </div>
-    </form>
-    <ul class="list-group mt-4">
-      <li class="list-group-item" v-for="(task, index) in getFilteredTasks" :key="index">
-        <input @change="event => task.done = event.target.checked" v-model="task.done" :id="task.title" type="checkbox" name="" id="">
-        <label :for="task.title" :class="{ done: task.done }" class="ms-3">{{ task.title }}</label>
-      </li>
-    </ul>
+    <Header :pendentsTasks="getPendentTasks.length" />
+    <Formulario :tempTask="state.tempTask" :registerTask="registerTask" :changeFilter="event => state.filter = event.target.value" :changeTempTask="event => state.tempTask = event.target.value" />
+    <Lista :filteredTasks="getFilteredTasks" />
   </div>
 </template>
 
 <style scoped>
-  .done {
-    text-decoration: line-through;
-  }
+
 </style>
---
+++ todo-vue/src/components/Formulario.vue
@@ -0,0 +1,23 @@
+<script setup>
+    const props = defineProps(['registerTask', 'tempTask', 'changeFilter', 'changeTempTask'])
+</script>
+
+<template>
+    <form @submit.prevent="props.registerTask" action="">
+      <div class="row">
+        <div class="col">
+          <input :value="props.tempTask" @change="props.changeTempTask" required type="text" name="" id="" class="form-control" placeholder="Digite aqui a descrição da sua tarefa">
+        </div>
+        <div class="col-md-2">
+          <button type="submit" class="btn btn-primary">Cadastrar</button>
+        </div>
+        <div class="col-md-2">
+          <select @change="props.changeFilter" name="" id="" class="form-control">
+            <option value="todas">Todas as tarefas</option>
+            <option value="pendentes">Pendentes</option>
+            <option value="finalizadas">Finalizadas</option>
+          </select>
+        </div>
+      </div>
+    </form>
+</template>
